59a029ceff57a37bf5eaac9d5048e260fcc5c244
Pedro Alexandre Romano Lopes <pedroalexlopes25@gmail.com>
2020-06-28 12:50:49 +0100

    Improve CSS so it looks neater

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -8,14 +8,14 @@
              placeholder="add new todo">
       <img class="add-todo-image" @click="addTodo" src="../assets/add.png"/>
     </div>
-    <div>
+    <div class="todo-items-grid">
       <ul class="clean-list">
-        <li class="todo-items" v-for="todo in todos" :key="todo">
+        <li class="todo-items" v-for="todo in todos" :key="todo.text">
           <input class="todo-checkbox"
-                 id="checkbox"
-                 type="checkbox"
-                 v-model="todo.completed"
-                 @change="deleteTodo(todo)" />
+             id="checkbox"
+             type="checkbox"
+             v-model="todo.completed"
+             @change="deleteTodo(todo)" />
           <label class="todo-item" for="checkbox">{{ todo.text }}</label>
         </li>
       </ul>
@@ -57,7 +57,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1 {
-  position: center;
   border: lightskyblue solid 2px;
   padding: 10px;
   display: inline-block;
@@ -66,66 +65,68 @@ h1 {
 
 .clean-list {
   list-style: none;
+  padding-left: 0;
 }
 
 .new-todo {
   display: inline-block;
+  width: 50%;
 }
 
 .new-todo:after {
   content: "";
   display: table;
   clear: both;
+  width: 70%;
 }
 
 .todo-input {
   font-size: 20px;
-  padding: 10px 10px 0px 10px;
+  padding: 10px 60px 5px 20px;
   height: 40px;
-  width: 300px;
+  width: 85%;
   border-radius: 45px;
-  border-right: 0;
   float: left;
+  border: solid lightskyblue 3px;
 }
 
 .todo-input::placeholder {
   font-size: 20px;
   font-style: italic;
   font-stretch: expanded;
+  margin-right: 55px;
 }
 
 .todo-input:focus {
   outline: none;
 }
 
+.add-todo-image {
+  width: 65px;
+  height: 65px;
+  float: left;
+  margin-left: -60px;
+  margin-top: -2px;
+}
+
 .todo-checkbox {
   margin-right: 10px;
 }
 
-.add-todo-image {
-  width: 58px;
-  height: 58px;
-  float: left;
-  margin-left: -54px;
-  margin-top: -2px;
+.todo-items-grid{
+  display: table;
+  margin: 0 auto;
+  width: 50%;
 }
 
 .todo-items{
-  margin-topstuff: 5px;
-  width: 200px;
-  background-color: azure;
+  margin-top: 5px;
   text-align: left;
-  list-style-position: inside;
-  color: dimgrey;
+  color: slategrey;
 }
 
 .todo-item {
-  margin-top: 5px;
-  display: inline-block;
-  background-color: azure;
-  text-align: left;
-  text-align: left;
-  list-style-position: inside;
+
 }
 
 </style>
